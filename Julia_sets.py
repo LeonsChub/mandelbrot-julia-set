0ac4b3654e35ea776d82719cfb5d6ff767bace22
Leon <pavlenkoleon@gmail.com>
2022-06-23 19:28:51 +0300

    mandelbrot v1.1 with julia sets

---
+++ Julia_sets.py
@@ -0,0 +1,130 @@
+from turtle import color
+import numpy as np
+import pygame
+import fractal_engine as fe
+from pygame import gfxdraw
+
+pallete = []
+
+iter = 15
+
+for i in range(iter):
+    color_step = 255/iter
+    pallete.append((color_step*i,color_step*i,color_step*i))
+
+
+pygame.init()
+clock = pygame.time.Clock() #pygame clock for fps
+
+width = 300 # screen parameters
+height = 300
+
+screen = pygame.display.set_mode([width, height]) #init screen and fill it
+screen.fill((0, 0, 0))
+
+zoom = 4 #arbitrary number
+dr = zoom/width #set difference in real part
+di = -zoom/height #set difference in imag part
+
+anchor_pos = -2+2j #anchor position top left
+
+def px_to_coord(pos): # translate a pixel coordinate from the screen to a cartesian coordinate using steps an an anchor point
+    x , y = pos #pos tuple broken to vars x and y
+
+    real_part = anchor_pos.real + dr * x #anchor pos + steps(dr || di) * pixels pos
+    imag_part = anchor_pos.imag + di * y
+    
+    return(real_part+imag_part*1j)
+
+
+def init_coords(res = 1): # res stands for resolution you can turn it to 2 in order to cut the resolution in half. init coords with desired resolution
+    number_plain = np.ones((width,height),dtype=tuple) #numpy array maps every pixel to coordinate
+
+    res = int(res)
+
+    w = int(width/res)
+    h = int(height/res)
+
+
+    for i in range(w):
+        for q in range(h):
+
+            for a in range(res):
+                number_plain[res*i-a][res*q]   = (i * res , q * res)
+                for b in range(res):
+                    number_plain[res*i-a][res*q - b]   = (i * res , q * res)
+
+
+    vfunc = np.vectorize(px_to_coord) 
+    array = vfunc(number_plain)# maps each pixel coordinate to an imaginary coordinate using numpys vectorize function
+    
+    return array
+
+if __name__ == "__main__":
+
+    running = 1 
+
+    mapped_numbers = init_coords()
+
+    drag = False
+
+    
+
+    while running:
+
+        
+
+        m_pos = pygame.mouse.get_pos()
+        
+        
+        clock.tick(60)     
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = 0
+                
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    drag = True
+                    
+                    
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    drag = False
+                    
+
+        
+        lmb,cmb,rmb = pygame.mouse.get_pressed(num_buttons=3)
+
+        if(lmb):
+            c_anchor = px_to_coord(m_pos)
+            init_coords(1)
+            for i in range(width):
+                for q in range(height):
+                    z_n = mapped_numbers[i][q]
+                    intese = fe.repeat_iteration(z_n,iter,c_anchor)
+                    gfxdraw.pixel(screen,i,q,pallete[intese])
+
+        else:
+            for i in range(width):
+                for q in range(height):
+                    z_n = mapped_numbers[i][q]
+                    intese = fe.repeat_iteration(z_n,iter)
+                    gfxdraw.pixel(screen,i,q,pallete[intese])
+
+            
+
+        
+        
+
+        #total_time = time.time() - start_time
+        #print(str(total_time )+": SECONDS")
+
+        
+
+        pygame.display.flip()       
+
+    pygame.quit()
+
+    
+
+
---
+++ fractal_engine.py
@@ -0,0 +1,51 @@
+import time
+import numpy as np
+from numba import njit
+
+# mandelbrot equation {z(new) = z(old)^2 + c}    z = a + bj
+@njit
+def z_iterate(i_num , c ): #iterate once through the mandelbrot equation
+
+    new_z = c + i_num**2
+
+    return new_z ,c
+
+@njit
+def repeat_iteration(i_num,lim ,c = 0): #iterate many times through the mandelbrot equation (lim amount of times) and return when the iteration breaks when it breaches a distance greater than two from the origin
+    z = i_num 
+    
+    if c == 0:
+        constant = z
+    
+    else:
+        constant = c
+
+    for i in range(lim):
+        z,constant = z_iterate(z,constant) #gets resulting z and pushes constant to new function
+        if (get_dist(z) > 4): # if the distance of the point from the origin is greater than 2 return where the limit "escaped" note the distance isnt squared in order to save time
+            
+            return i
+
+    return i
+        
+@njit
+def get_dist(z_num):
+    x = z_num.real
+    y = z_num.imag
+
+    dist = x**2 + y **2 # break down imaginary and real part pythagorean therom  to find the distance 
+
+    return dist
+        
+
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+    print("STARTING TIME")
+
+    print(repeat_iteration(1+0.25j,10000))
+
+    total_time = time.time() - start_time
+    print(str(total_time )+": SECONDS")
+
